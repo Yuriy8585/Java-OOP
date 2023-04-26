5f017accc920391587b140e675c66e409cd8846d
Yuriy Krasnov <118444989+Yuriy8585@users.noreply.github.com>
2023-04-26 17:36:01 +0200

    task

--- Homework3/Main.java
+++ Homework3/Main.java
@@ -9,11 +9,11 @@ public class Main {
     private static Random random = new Random();
 
     static Employee generateEmployee() {
-        String[] names = new String[] { "Иван", "Петр", "Василий", "Олег", "Михаил" };
-        String[] surNames = new String[] { "Иванов", "Петров", "Васильев", "Соколов", "Воробьев" };
+        String[] names = new String[] { "Иван", "Петр", "Василий", "Юрий", "Михаил" };
+        String[] surNames = new String[] { "Иванов", "Петров", "Сидоров", "Краснов", "Перфильев" };
         int salary = random.nextInt(200, 300);
         int index = random.nextInt(30, 50);
-        int age = random.nextInt(25, 50);
+        int age = random.nextInt(25, 55);
         if (random.nextBoolean())
             return new Worker(names[random.nextInt(5)], surNames[random.nextInt(5)], age, salary * index);
         else
